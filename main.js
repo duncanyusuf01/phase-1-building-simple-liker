a4cc009a7e6a11c02ce439fbb9b2e01cd1bdbfa5
Duncan Kariuki <duncankariukimugo@gmail.com>
2025-03-25 09:28:08 +0300

    adds main.js

--- main.js
+++ main.js
@@ -3,7 +3,36 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const hearts=document.querySelectorAll('.like-glyph');
+hearts.forEach(heart => {
+  heart.addEventListener('click', ()=>{
+    handleHeartClick(heart);
+  })
+})
+function handleHeartClick(heart) {
+  mimicServerCall()
+    .then(() => {
+      // Server request was successful
+      if (heart.textContent === "♡") {
+        heart.textContent = "♥"; // Change to full heart
+        heart.classList.add("activated-heart"); // Make it red
+      } else {
+        heart.textContent = "♡"; // Change back to empty heart
+        heart.classList.remove("activated-heart"); // Remove red color
+      }
+    })
+    .catch(() => {
+      // Server request failed - Show error modal
+      const errorModal = document.getElementById("modal");
+      errorModal.classList.remove("hidden"); // Show error modal
+      errorModal.querySelector("#modal-message").textContent = "Server error. Try again!";
 
+      // Hide error modal after 3 seconds
+      setTimeout(() => {
+        errorModal.classList.add("hidden");
+      }, 3000);
+    });
+}
 
 
 
